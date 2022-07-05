b15f8a5a571eae030274f75ee24e05cdf8e814e4
jinsu291 <wlstn291@naver.com>
2022-07-06 01:06:31 +0900

    a01 추가

---
+++ src/main/java/a01_Hello/PrintEx.java
@@ -0,0 +1,27 @@
+package a01_Hello;
+
+public class PrintEx {
+    public static void main(String[] args) {
+        //과제1
+        System.out.println("이름 : 서진수");
+        System.out.println("연락처 : 010 - 0000 - 0000");
+        System.out.println("이메일 : 0000@naver.com");
+        System.out.println("주소 : 부산광역시 사하구 신평동");
+        System.out.println(" ");
+
+        //과제2
+        System.out.println("The world’s most popular modern development platform, Java SE");
+        System.out.println("reduces costs, shortens development time, drives innovation, and");
+        System.out.println("improves application services as the programming language of");
+        System.out.println("Oracle Java SE Subscription, which now includes GraalVM");
+        System.out.println("Enterprise and Java Management Service for free.");
+        System.out.println(" ");
+
+        //문제 1
+        System.out.println("이름 : 서진수");
+        System.out.println("연락처 : 010 - 0000 - 0000");
+        System.out.println("이메일 : 0000@naver.com");
+        System.out.println("주소 : 부산광역시 사하구 신평동");
+    }
+
+}
