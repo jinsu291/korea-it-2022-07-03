6138170f4c1c9d4946f7d23b09f3df88e6c832e7
jinsu291 <wlstn291@naver.com>
2022-07-06 10:40:55 +0900

    a04 수정

--- src/main/java/a04_형변환/TypeConversionEx1.java
+++ src/main/java/a04_형변환/TypeConversionEx1.java
@@ -2,12 +2,31 @@ package a04_형변환;
 
 public class TypeConversionEx1 {
     public static void main(String[] args) {
-        double a = 5;
-        int b = 2;
 
-        System.out.println("나누기" + a / b);
+        //문자 -> 상수
 
-        System.out.println("나머지" + a % b);
-        //나머지 활용 : 배수, 홀수, 짝수
+        // 업캐스팅
+        char a = 'a';
+        int b = a;
+        int c = 'c';
+        System.out.println("문자 -> 정수: " + b);
+
+        //다운캐스팅
+        int d = 1000;
+        char e = (char)d;
+        System.out.println("정수 -> 문자: " + e);
+
+        //정수 -> 실수
+
+        //업캐스팅
+        int f = 5000;
+        double g = f;
+        System.out.println("정수 -> 실수: " + g);
+
+        //다운캐스팅
+        double h = 30.5533;
+        int i = (int)h;
+        System.out.println("실수 -> 정수: " + i);
     }
+
 }
