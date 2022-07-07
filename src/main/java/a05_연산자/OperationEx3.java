f7f3c81c51139e513fec06271124f861df9663ef
jinsu291 <wlstn291@naver.com>
2022-07-07 17:20:42 +0900

    a05 3추가

---
+++ src/main/java/a05_연산자/OperationEx3.java
@@ -0,0 +1,16 @@
+package a05_연산자;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        boolean result1 = 10 < 5;
+        System.out.println("result1 : " + result1);
+
+        boolean result2 = 10 == 10;
+        System.out.println("result2 : " + result2);
+
+        boolean result3 = 10 != 11;
+        System.out.println("result3 : " + result3);
+    }
+
+}
+
