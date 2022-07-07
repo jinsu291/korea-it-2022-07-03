99bbe6d70ae7b0d1fcb5a31accf3e1babcd11dbd
jinsu291 <wlstn291@naver.com>
2022-07-07 17:03:47 +0900

    a05 1추가

---
+++ src/main/java/a05_연산자/OperationEx1.java
@@ -0,0 +1,12 @@
+package a05_연산자;
+
+public class OperationEx1 {
+    public static void main(String[] args) {
+        double a = 5;
+        int b = 2;
+
+        System.out.println("나누기" + a / b);
+        System.out.println("나머지" + a % b);
+        // 나머지 활용 : 배수, 홀수, 짝수
+    }
+}
