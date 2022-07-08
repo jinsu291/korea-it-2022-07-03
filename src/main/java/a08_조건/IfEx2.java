1d68fe150625e3f373acbc997d97a258bc905fe2
jinsu291 <wlstn291@naver.com>
2022-07-09 01:08:24 +0900

    a08 2추가

---
+++ src/main/java/a08_조건/IfEx2.java
@@ -0,0 +1,14 @@
+package a08_조건;
+
+import java.util.Scanner;
+
+public class IfEx2 {
+    public static void main(String[] args) {
+
+        // if (조건식) 헤딩 조건이 참일때 실행문
+        if(true) System.out.println("참입니다.");
+        if (false) System.out.println("이쪽은 실행 안됨");
+        else System.out.println("거짓일때 실행됨");
+        // else 해당 조건이 거짓일 때 실행
+    }
+}
