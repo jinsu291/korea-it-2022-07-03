5260531c6eb652e15c2a483cb622ccc299955685
jinsu291 <wlstn291@naver.com>
2022-07-09 04:45:18 +0900

    a09 2추가

---
+++ src/main/java/a09_반복/DoWhileEx.java
@@ -0,0 +1,11 @@
+package a09_반복;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println(i);
+        } while (i == 0);
+    }
+}
