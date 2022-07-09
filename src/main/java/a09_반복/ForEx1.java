f3884869a1ab1682ab1b09a02c7f8adb81663910
jinsu291 <wlstn291@naver.com>
2022-07-10 00:08:17 +0900

    a09 3추가

---
+++ src/main/java/a09_반복/ForEx1.java
@@ -0,0 +1,11 @@
+package a09_반복;
+
+public class ForEx1 {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i < 10; i++){
+            System.out.println(i);
+            System.out.println(i+1);
+        }
+    }
+}
